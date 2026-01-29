005a0bcd2f82684a0840cf90024ccbeae1c14fe4
Daniel Vygolov <rattack@gmail.com>
2026-01-29 13:28:38 +0400

    refactoring + users from recent actions = hashset instead of list

--- Copyright.cs
+++ Copyright.cs
@@ -4,8 +4,7 @@
     {
         public static void Print()
         {
-
-            Console.WriteLine(@"                Telegram Bots Cleaner v2.1.0 ");
+            Console.WriteLine(@"                Telegram Bots Cleaner v2.2.0 ");
             Console.WriteLine(@"   _            __     __  _ _             __          __  _     ");
             Console.WriteLine(@"  | |           \ \   / / | | |            \ \        / / | |    ");
             Console.WriteLine(@"  | |__  _   _   \ \_/ /__| | | _____      _\ \  /\  / /__| |__  ");
@@ -15,7 +14,7 @@
             Console.WriteLine(@"          __/ |                                                  ");
             Console.WriteLine(@"         |___/                  https://yellowweb.top            ");
             Console.WriteLine();
-            Console.WriteLine("If you like this software, please, donate!");
+            Console.WriteLine("If you like this software, please, DONATE!");
             Console.WriteLine("USDT TRC20: TKeNEVndhPSKXuYmpEwF4fVtWUvfCnWmra");
             Console.WriteLine("Bitcoin: bc1qqv99jasckntqnk0pkjnrjtpwu0yurm0qd0gnqv");
             Console.WriteLine("Ethereum: 0xBC118D3FDE78eE393A154C29A4545c575506ad6B");
--- Program.cs
+++ Program.cs
@@ -1,119 +1,133 @@
 ﻿using System.Globalization;
 using TG.GroupBotsCleaner;
 using TL;
+//Supressing excessive TG logging
+WTelegram.Helpers.Log = (lvl, str) => System.Diagnostics.Debug.WriteLine(str);
 Copyright.Print();
 
 Console.Write("Enter your phone number, for example +79222045502:");
 var yourPhone = Console.ReadLine() ?? "";
-Console.Write("Enter your chat/channel name without @, for example ohmyctr:");
-var chatName = Console.ReadLine();
 Console.Write("Enter your app id from https://my.telegram.org/apps:");
 var apiId = int.Parse(Console.ReadLine() ?? "-1");
 Console.Write("Enter your app hash from https://my.telegram.org/apps:");
-var apiHash = Console.ReadLine();
+string apiHash = Console.ReadLine() ?? "";
 Console.Write("Enter your 2FA key, if required. Otherwise just press Enter:");
-var twofaSecret = Console.ReadLine();
+string twofaSecret = Console.ReadLine()??"";
 
-string format = "yyyy-MM-dd HH:mm:ss";
-Console.WriteLine("Enter start date and time to clear users in UTC0 timezone, for example 2024-11-22 22:00:00");
-var startDate = Console.ReadLine() ?? "";
-DateTime.TryParseExact(startDate, format, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal | DateTimeStyles.AdjustToUniversal, out DateTime minDate);
-Console.WriteLine("Enter end date and time to clear users in UTC0 timezone, for example 2024-11-22 22:15:00");
-var endDate = Console.ReadLine() ?? "";
-DateTime.TryParseExact(endDate, format, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal | DateTimeStyles.AdjustToUniversal, out DateTime maxDate);
+var client = await ConfigureClientAsync(apiId, apiHash, yourPhone, twofaSecret);
 
-Console.WriteLine("Starting to work...");
-
-//Do not modify anything starting from here!
-//Supressing excessive logging
-WTelegram.Helpers.Log = (lvl, str) => System.Diagnostics.Debug.WriteLine(str);
-using var client = new WTelegram.Client(apiId, apiHash);
-await DoLogin(yourPhone);
-
-//Gettings our chat
-Console.WriteLine("Getting ALL chats, please WAIT...");
-var chats = await client.Messages_GetAllChats();
-var chat = chats.chats.Values.Where(c => c is Channel).Cast<Channel>().FirstOrDefault(c => c.username == chatName);
-if (chat == null)
+var answer = "Y";
+while (answer.ToLower() == "y")
 {
-    Console.WriteLine($"No chat/channel with name {chatName} found!!!");
-    return;
-}
-Console.WriteLine($"Found chat to work on: {chat.ID} - {chat.Title}");
-Console.WriteLine($"Getting ALL chat/channel members, please WAIT...");
-//Getting all chat members
-var participants = await client.Channels_GetAllParticipants(chat);
-Console.WriteLine($"Found {participants.count} users in this chat/channel");
-//If we already save users - load them, else get them from Recent Acitons log
-List<long>? users = null;
-if (File.Exists("users.txt"))
-{
-    Console.WriteLine("Found users.txt file, reading users that will be banned from there...");
-    users = File.ReadAllLines("users.txt").Select(long.Parse).ToList();
-    Console.WriteLine($"Found {users.Count} users.");
+    Console.Write("Enter your chat/channel name without @, for example ohmyctr:");
+    var chatName = Console.ReadLine() ?? "";
+
+    string format = "yyyy-MM-dd HH:mm:ss";
+    Console.WriteLine("Enter start date and time to clear users in UTC0 timezone, for example 2024-11-22 22:00:00");
+    var startDate = Console.ReadLine() ?? "";
+    DateTime.TryParseExact(startDate, format, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal | DateTimeStyles.AdjustToUniversal, out DateTime minDate);
+    Console.WriteLine("Enter end date and time to clear users in UTC0 timezone, for example 2024-11-22 22:15:00");
+    var endDate = Console.ReadLine() ?? "";
+    DateTime.TryParseExact(endDate, format, CultureInfo.InvariantCulture, DateTimeStyles.AssumeUniversal | DateTimeStyles.AdjustToUniversal, out DateTime maxDate);
+
+    await CleanAsync(client, chatName, minDate, maxDate);
+    Console.Write("Do you want to clean another chat or channel?(Y/N):");
+    answer = Console.ReadLine() ?? "N";
 }
 
-if (users == null)
-{
-    //Getting Recent Actions log and filling new members list
-    var filter = new ChannelAdminLogEventsFilter() { flags = ChannelAdminLogEventsFilter.Flags.join };
-    long maxId = long.MaxValue;
-    if (File.Exists("maxid.txt"))
-    {
-        Console.WriteLine("Found maxid.txt file, reading max id from there.");
-        maxId = long.Parse(File.ReadAllText("maxid.txt"));
-    }
-
-    Console.WriteLine($"Current MaxId: {maxId}");
-    users = new List<long>();
-    Console.WriteLine("Getting part of the log...");
-    Channels_AdminLogResults log;
-    do
-    {
-        log = await client.Channels_GetAdminLog(chat, string.Empty, events_filter: filter, max_id: maxId);
-        if (log.events.Length == 0) break;
-        var events = log.events.Where(ev => ev.date >= minDate && ev.date < maxDate).ToList();
-        users.AddRange(events.Select(ev => ev.user_id));
-        maxId = log.events.Last().id;
-        File.WriteAllText("maxid.txt", maxId.ToString());
-    }
-    while (log.events.First().date >= minDate);
-    File.WriteAllLines("users.txt", users.Select(u => u.ToString()).ToArray());
-}
-
-//Cleaning all users
-while (users.Count > 0)
-{
-    try
-    {
-        if (!participants.users.ContainsKey(users[0]))
-        {
-            users.RemoveAt(0);
-            continue;
-        }
-
-        var tgUser = participants.users[users[0]];
-        Console.WriteLine($"Removing user: {tgUser.MainUsername} with id {users[0]}...");
-        var delRes = await client.DeleteChatUser(chat,tgUser);
-    }
-    catch (RpcException ex)
-    {
-        Console.WriteLine($"{DateTime.Now:HH:mm:ss} Got flood warning, sleeping for {ex.X} seconds till {DateTime.Now.AddSeconds(ex.X):HH:mm:ss}...");
-        await Task.Delay(ex.X * 1000);
-        Console.WriteLine("Work continued!");
-        var delRes = await client.DeleteChatUser(chat, participants.users[users[0]]);
-    }
-    users.RemoveAt(0);
-    File.WriteAllLines("users.txt", users.Select(u => u.ToString()).ToArray());
-}
 Console.WriteLine("All that could be cleaned is cleaned! Thank you for using my script!");
 Console.WriteLine("You can donate me smth here USDT TRC20: TKeNEVndhPSKXuYmpEwF4fVtWUvfCnWmra");
 File.Delete("users.txt");
 File.Delete("maxid.txt");
 
-
-async Task DoLogin(string? loginInfo)
+async Task CleanAsync(WTelegram.Client client, string chatName, DateTime minDate, DateTime maxDate)
 {
+    Console.WriteLine("Starting to work...");
+
+    //Gettings our chat
+    Console.WriteLine("Getting ALL chats, please WAIT...");
+    var chats = await client.Messages_GetAllChats();
+    var chat = chats.chats.Values.Where(c => c is Channel).Cast<Channel>().FirstOrDefault(c => c.username == chatName);
+    if (chat == null)
+    {
+        Console.WriteLine($"No chat/channel with name {chatName} found!!!");
+        return;
+    }
+    Console.WriteLine($"Found chat to work on: {chat.ID} - {chat.Title}");
+    Console.WriteLine($"Getting ALL chat/channel members, please WAIT...");
+    //Getting all chat members
+    var participants = await client.Channels_GetAllParticipants(chat);
+    Console.WriteLine($"Found {participants.count} users in this chat/channel");
+    //If we already save users - load them, else get them from Recent Acitons log
+    HashSet<long>? users = null;
+    if (File.Exists("users.txt"))
+    {
+        Console.WriteLine("Found users.txt file, reading users that will be banned from there...");
+        users = File.ReadAllLines("users.txt").Select(long.Parse).ToHashSet();
+        Console.WriteLine($"Found {users.Count} users.");
+    }
+
+    if (users == null)
+    {
+        //Getting Recent Actions log and filling new members list
+        var filter = new ChannelAdminLogEventsFilter() { flags = ChannelAdminLogEventsFilter.Flags.join };
+        long maxId = long.MaxValue;
+        if (File.Exists("maxid.txt"))
+        {
+            Console.WriteLine("Found maxid.txt file, reading max id from there.");
+            maxId = long.Parse(File.ReadAllText("maxid.txt"));
+        }
+
+        Console.WriteLine($"Current MaxId: {maxId}");
+        users = new HashSet<long>();
+        Console.WriteLine("Getting part of the log...");
+        Channels_AdminLogResults log;
+        do
+        {
+            log = await client.Channels_GetAdminLog(chat, string.Empty, events_filter: filter, max_id: maxId);
+            if (log.events.Length == 0) break;
+            var events = log.events.Where(ev => ev.date >= minDate && ev.date < maxDate).ToList();
+            users.UnionWith(events.Select(ev => ev.user_id));
+            maxId = log.events.Last().id;
+            File.WriteAllText("maxid.txt", maxId.ToString());
+        }
+        while (log.events.First().date >= minDate);
+        File.WriteAllLines("users.txt", users.Select(u => u.ToString()).ToArray());
+    }
+
+    //Cleaning all users
+    while (users.Count > 0)
+    {
+        var firstUser = users.First();
+        try
+        {
+            if (!participants.users.ContainsKey(firstUser))
+            {
+                users.Remove(firstUser);
+                continue;
+            }
+
+            var tgUser = participants.users[firstUser];
+            Console.WriteLine($"Removing user: {tgUser.MainUsername} with id {firstUser}...");
+            var delRes = await client.DeleteChatUser(chat,tgUser);
+        }
+        catch (RpcException ex)
+        {
+            Console.WriteLine($"{DateTime.Now:HH:mm:ss} Got flood warning, sleeping for {ex.X} seconds till {DateTime.Now.AddSeconds(ex.X):HH:mm:ss}...");
+            await Task.Delay(ex.X * 1000);
+            Console.WriteLine("Work continued!");
+            var delRes = await client.DeleteChatUser(chat, participants.users[firstUser]);
+        }
+        users.Remove(firstUser);
+        File.WriteAllLines("users.txt", users.Select(u => u.ToString()).ToArray());
+    }
+}
+
+
+async Task<WTelegram.Client> ConfigureClientAsync(int apiId, string apiHash, string phone, string twofaSecret)
+{
+    var client = new WTelegram.Client(apiId, apiHash);
+    var loginInfo = phone;
     while (client.User == null)
         switch (await client.Login(loginInfo))
         {
@@ -133,4 +147,5 @@ async Task DoLogin(string? loginInfo)
                 break;
         }
     Console.WriteLine($"We are logged-in as {client.User} (id {client.User.id})");
+    return client;
 }
